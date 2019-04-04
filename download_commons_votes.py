ad5f6d07698f7d4c12dc4a9178b461cdd815735e
hdvos <hdvos93@gmail.com>
2019-04-04 15:21:57 +0200

    repair downlaod commons votes

--- download_commons_votes.py
+++ download_commons_votes.py
@@ -1,17 +1,18 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+"""A script that downloads voting records from the data portal of the house of commons.
 
+TODO: Read parameters (like location) from command line
+TODO: only download new files. Do not download files that are allready downloaded.
+"""
+from urllib.request import urlretrieve
 
-with webdriver.Firefox() as driver:
-    driver.get("https://commonsvotes.digiminster.com/")
-    for i in range(0, 665):
-        driver.get(f"https://commonsvotes.digiminster.com/Divisions/Details/{i}")
-        src = driver.page_source
-        # print(src)
-        if "This system has suffered an error" in src:
-            print('skip')
-            continue
-        download_button_1 = driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div[3]/div/div[1]/div/div[4]/div/div/a')
-        download_button_1.click()
-        
-        input("next")
+if __name__ == '__main__':
+    for i in range(1000):
+        url = f"https://commonsvotes.digiminster.com/Divisions/DownloadCSV/{i}" # The url that leads to a document.
+
+        location = f'csv_files/{i:04}.csv'      # How the document will be saved
+
+        try:    
+            urlretrieve(url, location)
+            print(f"File stored as {location}")
+        except Exception as e:                  # An exception will be raised if the url does not exist.
+            print(e)
